bb36ee7abe430999412f8fe6855a7898cb0cc1c9
MoritzChristoffers <moritz.christoffers2911@gmail.com>
2021-06-13 13:19:27 +0200

    commit

--- Area51.py
+++ Area51.py
@@ -12,8 +12,8 @@ api = pyyoutube.Api(api_key='AIzaSyCPv-GvwuO6k-VlPuX_Ki8ZmGlDdaN-DlM')
 my_path = os.path.abspath(os.path.dirname(__file__))
 steal_json = Json.StealYoutubeVideoJson()
 steal_json.Youtube_Uri = 'https://www.youtube.com/watch?v=UZm8jB3wtQE'
-steal_json.Downloaded_Path = os.path.join(my_path, 'Videos/test')
-steal_json.Branded_Path = os.path.join(my_path, 'Videos/test_branded')
+steal_json.Downloaded_Path = os.path.join(my_path, 'Download/test')
+steal_json.Branded_Path = os.path.join(my_path, 'Download/test_branded')
 branding_json = Json.BrandingJson()
 branding_json.Intro_Start_End = [60, 60 + 25]
 steal = Jobs.StealYoutubeVideo(steal_json)
